12dca000556e6abe2950c7e9eb6c07692227157e
Frans Peter Larsen <larsen.frans@gmail.com>
2017-11-30 14:06:44 +0100

    Init Draw fn for Voxel

--- Ex6/Voxel.cpp
+++ Ex6/Voxel.cpp
@@ -1,1 +1,17 @@
 #include "Voxel.hpp"
+
+Voxel::Voxel() {
+	type = VoxelType::Rock;
+}
+
+Voxel::~Voxel() {
+
+}
+
+void Voxel::draw(sre::RenderPass& pass) {
+
+}
+
+void Voxel::setType(VoxelType type) {
+	this->type = type;
+}
--- Ex6/Voxel.hpp
+++ Ex6/Voxel.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
+#include "sre/SDLRenderer.hpp"
+
 class Voxel {
 public:
 	Voxel();
 	~Voxel();
 
-	void draw();
-	void setType(VoxelType);
+	void draw(sre::RenderPass& renderpass);
+	void setType(VoxelType type);
 private:
 	VoxelType type;
 };
