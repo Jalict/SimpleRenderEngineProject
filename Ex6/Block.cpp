490ada78423ea908f96fdd20c0de7b78d7b7cfc3
Frans Peter Larsen <larsen.frans@gmail.com>
2017-12-01 13:16:45 +0100

    Initial TexCoords for Block

--- Ex6/Block.cpp
+++ Ex6/Block.cpp
@@ -7,9 +7,9 @@ Block::Block() {
 }
 
 Block::Block(BlockType type) {
-	this->type = type;
+	setType(type);
 
-	mesh->create().withCube(1.0f);
+	mesh->create().withCube(1.0f).withUVs(texCoords).build();
 }
 
 Block::~Block() {
@@ -18,6 +18,24 @@ Block::~Block() {
 
 void Block::setType(BlockType type) {
 	this->type = type;
+
+	glm::vec4 coords = textureCoordinates((int)type);
+
+	texCoords.clear();
+
+	texCoords.insert(texCoords.end(), {
+		glm::vec4(0,0,0,0), glm::vec4(0,0,0,0), glm::vec4(0,0,0,0),
+		glm::vec4(0,0,0,0), glm::vec4(0,0,0,0), glm::vec4(0,0,0,0),
+
+		glm::vec4(0,0,0,0), glm::vec4(0,0,0,0), glm::vec4(0,0,0,0),
+		glm::vec4(0,0,0,0), glm::vec4(0,0,0,0), glm::vec4(0,0,0,0),
+
+		glm::vec4(0,0,0,0), glm::vec4(0,0,0,0), glm::vec4(0,0,0,0),
+		glm::vec4(0,0,0,0), glm::vec4(0,0,0,0), glm::vec4(0,0,0,0),
+
+		glm::vec4(0,0,0,0), glm::vec4(0,0,0,0), glm::vec4(0,0,0,0),
+		glm::vec4(0,0,0,0), glm::vec4(0,0,0,0), glm::vec4(0,0,0,0)
+	});
 }
 
 std::shared_ptr<sre::Mesh> Block::getMesh()
@@ -25,8 +43,23 @@ std::shared_ptr<sre::Mesh> Block::getMesh()
 	return mesh;
 }
 
-std::shared_ptr<sre::Material> Block::getMaterial()
-{
-	return material;
-}
+//(TODO) Move to somewhere more generic and make it static
+// FPL, 1 December 2017
+glm::vec4 Block::textureCoordinates(int blockID) {
+	glm::vec2 textureSize(1024, 2048);
+	glm::vec2 tileSize(128, 128);
 
+	float tileWidth = tileSize.x / textureSize.x;
+	float tileHeight = tileSize.y / textureSize.y;
+
+	glm::vec2 min = glm::vec2(0, 16 * tileSize.y) / textureSize;
+	glm::vec2 max = min + tileSize / textureSize;
+
+	min.x += (blockID % 8) * tileWidth * 2;
+	max.x += (blockID % 8) * tileWidth * 2;
+
+	min.y -= ((blockID - (blockID % 8)) / 8) * tileHeight;
+	max.y -= ((blockID - (blockID % 8)) / 8) * tileHeight;
+
+	return glm::vec4(min.x, min.y, max.x, max.y);
+}
