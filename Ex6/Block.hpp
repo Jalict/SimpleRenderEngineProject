9d87afae5452be0e69a14f3cc68236790936f642
Frans Peter Larsen <larsen.frans@gmail.com>
2017-12-01 14:26:57 +0100

    Fixed block not being instantiated using MeshBuilder

--- Ex6/Block.hpp
+++ Ex6/Block.hpp
@@ -9,20 +9,20 @@ A block in the world
 */
 
 class Block {
-private:
+public:
 	enum BlockType { Rock, Grass };				// Types of blocks
+
+	Block();
+	Block(BlockType type);
+	~Block();
+
+	void setType(BlockType type);				// Change type of block
+	std::shared_ptr<sre::Mesh> getMesh();		// Get Mesh
+private:
 	BlockType type = BlockType::Rock;			// Current type of block
 
 	std::shared_ptr<sre::Mesh> mesh;			// Mesh of block
 	std::vector<glm::vec4> texCoords;	// texCoords for block
 
 	glm::vec4 textureCoordinates(int blockID);
-public:
-	Block();
-	Block(BlockType type);
-	~Block();
-
-	void setType(BlockType type);				// Change type of block
-
-	std::shared_ptr<sre::Mesh> getMesh();		// Get Mesh of Block
 };
