56ef2ce5e81801d111a4fe9375a556184e060c8c
Frans Peter Larsen <larsen.frans@gmail.com>
2017-12-08 12:07:47 +0100

    Particles actually move around now

--- Ex6/Chunk.cpp
+++ Ex6/Chunk.cpp
@@ -58,6 +58,15 @@ Chunk::Chunk(glm::vec3 position){
 
 	//assembleMeshData();
 	//createMesh();
+
+	// Particle System
+	particleTexture = sre::Texture::getWhiteTexture();
+	particleSystem = std::make_shared<ParticleSystem>(10, particleTexture);
+	particleSystem->gravity = { 0, -9.82, 0 };
+	particleSystem->material->setColor(glm::vec4(1, 0, 0, 1));
+	
+	updateApperance();
+	updateEmit();
 }
 
 Chunk::~Chunk(){
@@ -74,9 +83,7 @@ Chunk::~Chunk(){
 void Chunk::update(float dt) {
 
 	// Update particle systems
-	for (int i = 0; i < particleSystems.size(); i++) {
-		particleSystems[i]->update(dt);
-	}
+	particleSystem->update(dt);
 }
 
 //void Chunk::draw(sre::RenderPass& renderpass) {
@@ -135,10 +142,7 @@ void Chunk::draw(sre::RenderPass& renderpass) {
 	renderpass.draw(this->mesh, chunkTransform, Wolf3D::getInstance()->blockMaterial);
 
 	// Draw Particles
-	// Update particle systems
-	for (int i = 0; i < particleSystems.size(); i++) {
-		particleSystems[i]->draw(renderpass);
-	}
+	particleSystem->draw(renderpass);
 }
 
 
@@ -412,19 +416,35 @@ Block* Chunk::readBlock(int x, int y, int z) {
 		return nullptr;
 	}
 
-	//placeParticleSystem(glm::vec3(x + position.x, y + position.y, z + position.z));
+	//(TODO) Find better spot to emit particles
+	placeParticleSystem(glm::vec3(x + position.x, y + position.y, z + position.z));
 
 	// Else return the block
 	return &blocksInChunk[x][y][z];
 }
 
 void Chunk::placeParticleSystem(glm::vec3 pos) {
-	// Particle System
-	particleTexture = sre::Texture::getWhiteTexture();
-	particleSystems.insert(particleSystems.end(), std::make_shared<ParticleSystem>(10, particleTexture));
-	particleSystems[particleSystems.size() - 1]->gravity = { 0, -9.82, 0 };
-	particleSystems[particleSystems.size() - 1]->emitPosition = pos;
-	particleSystems[particleSystems.size() - 1]->emit();
+	//(TODO) Investigate weird spawning position
+	emitPosition = pos;
+
+	particleSystem->emit();
 
 	printf("created particle system at: %.2f,%.2f,%.2f\n", pos.x, pos.y, pos.z);
+}
+
+
+void Chunk::updateApperance() {
+	particleSystem->updateAppearance = [&](const Particle& p) {
+		p.size = glm::mix(sizeFrom, sizeTo, p.normalizedAge);
+	};
+}
+
+void Chunk::updateEmit() {
+	particleSystem->emitter = [&](Particle& p) {
+		p.position = emitPosition;
+		p.velocity = glm::sphericalRand(emitVelocity);
+		p.rotation = emitRotation;
+		p.angularVelocity = emitAngularVelocity;
+		p.size = sizeFrom;
+	};
 }
